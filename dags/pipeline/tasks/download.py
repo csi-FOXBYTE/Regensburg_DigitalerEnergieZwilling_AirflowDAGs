ac06f71810d07236fabc5a167dba843cb398e69a
Daniel Hoerner <Daniel.Hoerner@csi-online.de>
2026-03-09 17:05:22 +0100

    feat:  changed tasks

--- dags/pipeline/tasks/download.py
+++ dags/pipeline/tasks/download.py
@@ -1,5 +1,7 @@
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.sdk import Param
+from airflow.sdk.definitions.param import ParamsDict
 import os
 
 STORAGE_DIR_ON_HOST = os.getenv("CITYJSON_STORAGE_DIR", "/opt/airflow/data/storage")
@@ -14,8 +16,7 @@ def download_from_s3(bucket: str, key: str, dest: str):
     obj.download_file(dest)
 
 
-def download_task_callable(**context):
-    params = context["params"]
+def download_task_callable(params):
     if "bucket" not in params:
         raise ValueError(f"Missing required parameter: bucket")
     if "key" not in params:
@@ -32,10 +33,34 @@ def download_task_callable(**context):
     )
 
 
-def download() -> PythonOperator:
+def download(params: ParamsDict) -> PythonOperator:
     return PythonOperator(
         task_id="download_file_task",
         python_callable=download_task_callable,
-        provide_context=True,
+        op_kwargs={'params': params},
     )
     
+"""
+params=ParamsDict({
+        "bucket": Param(
+            default="",
+            type="string",
+            description="Name of the S3 bucket containing the object",
+        ),
+        "key": Param(
+            default="",
+            type="string",
+            description="Key (path) of the object in the bucket",
+        ),
+        "dest_dir": Param(
+            default="",
+            type="string",
+            description="Host directory to save the downloaded file (defaults to storage dir)",
+        ),
+        "target_filename": Param(
+            default="downloaded_file.zip",
+            type="string",
+            description="Filename to use for the downloaded object",
+        ),
+    }),
+"""
--- dags/pipeline/tasks/extract_zip.py
+++ dags/pipeline/tasks/extract_zip.py
@@ -1,27 +1,43 @@
 from airflow.providers.standard.operators.python import PythonOperator
+from airflow.sdk.definitions.param import ParamsDict
+from airflow.sdk import Param
 import os
 import zipfile
 
-# This method extracts the zip.
-def extractZip(src_dir: str, zip_name: str, dest_dir: str):
-    zip_path = os.path.join(src_dir, zip_name)
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(dest_dir)
-
+# This callable extracts the zip.
 def extract_zip_callable(**context):
     params = context["params"]
     src_dir = params.get("src_dir")
     zip_name = params.get("zip_name")
     dest_dir = params.get("dest_dir")
-    extract_zip(
-        src_dir=src_dir,
-        zip_name=zip_name,
-        dest_dir=dest_dir
-    )
+    zip_path = os.path.join(src_dir, zip_name)
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(dest_dir)
 
-def extract_zip() -> PythonOperator:
+def extract_zip(params: ParamsDict) -> PythonOperator:
     return PythonOperator(
         task_id="extract_zip",
         python_callable=extract_zip_callable,
         provide_context=True,
+        op_kwargs={'params': params}
     )
+
+"""
+params=ParamsDict({
+    "src_dir": Param(
+        default="",
+        type="string",
+        description="Name of the Directory where the Zip is that needs to be extracted",
+    ),
+    "dest_dir": Param(
+        default="",
+        type="string",
+        description="Name of the Directory where the extracted files will be",
+     ),
+    "zip_name": Param(
+        default="",
+        type="string",
+        description="Name of the Zip-file to extract",
+        ),
+}),
+"""
